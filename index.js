9803e40dce64f96a1d4644b98057b95de2dbe6c2
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-29 21:08:37 +0700

    update css

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
 let $ = document;
-const logos = [
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture3-2.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture1-2.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture2-2.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture21-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture15-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture4-2.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture5-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture28.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture9-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture13-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture7.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture8.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture11.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture16.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture18.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture14-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture6.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture19-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture20.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture10-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture22-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture26.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture23-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture24.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture12-1.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture25.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture27.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture29.png",
-    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture17.png"
-]
 
 
 // products page switch tabs
@@ -69,6 +38,38 @@ $.addEventListener('DOMContentLoaded', () => {
 
         lastScrollTop = scrollTop;
     })
+
+const logos = [
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture3-2.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture1-2.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture2-2.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture21-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture15-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture4-2.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture5-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture28.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture9-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture13-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture7.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture8.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture11.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture16.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture18.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture14-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture6.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture19-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture20.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture10-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture22-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture26.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture23-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture24.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture12-1.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture25.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture27.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture29.png",
+    "http://125.25.57.91:8081/wp-content/uploads/2023/08/Picture17.png"
+]
 
 const logoGrids = [
     { selector: ".logo-grid-1", initialIndex: 0, interval: 8000 },
@@ -112,44 +113,15 @@ const updateLogoGrid = (logoGrid, logos, currentIndex, interval) => {
 const intervals = {};
 
 const resetRandomInterval = (logoGrid, logos, currentIndex, interval) => {
-    if(intervals[logoGrid.className]){
-        clearInterval(intervals[logoGrid.className]);
+    if(intervals[logoGrid.classList]){
+        clearInterval(intervals[logoGrid.classList]);
     }
-
-    intervals[logoGrid.className] = setInterval(() => {
+    intervals[logoGrid.classList] = setInterval(() => {
         currentIndex = updateLogoGrid(logoGrid, logos, currentIndex, interval);
     }, interval);
 }
-
-// Using the function for different elements
-// let currentIdx1 = 0;
-// const logoGrid1 = document.querySelector(".logo-grid-1");
-// updateLogoGrid(logoGrid1, logos, currentIdx1, 5000);
-
-// let currentIdx2 = 2;
-// const logoGrid2 = document.querySelector(".logo-grid-2");
-// updateLogoGrid(logoGrid2, logos, currentIdx2, 4200);
-
-// let currentIdx3 = 4;
-// const logoGrid3 = $.querySelector('.logo-grid-3')
-// updateLogoGrid(logoGrid3, logos, currentIdx3, 6000)
-
-// let currentIdx4 = 6;
-// const logoGrid4 = $.querySelector('.logo-grid-4')
-// updateLogoGrid(logoGrid4, logos, currentIdx4, 3400)
-
-// let currentIdx5 = 8;
-// const logoGrid5 = $.querySelector('.logo-grid-5')
-// updateLogoGrid(logoGrid5, logos, currentIdx5, 5400)
-
-// let currentIdx6 = 10;
-// const logoGrid6 = $.querySelector('.logo-grid-6')
-// updateLogoGrid(logoGrid6, logos, currentIdx6, 6000)
 
 logoGrids.map(({ selector, initialIndex, interval }) => {
     let logoElement = $.querySelector(selector);
     updateLogoGrid(logoElement, logos, initialIndex, interval)
 })
-
-
-// ... and so on for other grids.
